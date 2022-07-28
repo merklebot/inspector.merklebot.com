34d94b362c320ad090cd3061e777442fc4a631c5
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-07-28 19:53:50 +0300

    Frontend: sidebar highlights active page

--- frontend/components/TheSidebar.vue
+++ frontend/components/TheSidebar.vue
@@ -1,9 +1,27 @@
 <template>
   <aside class="border-r-2">
     <ul class="menu w-56">
-      <li class="bordered"><NuxtLink to="/">Control panel</NuxtLink></li>
-      <li><NuxtLink to="">Planner</NuxtLink></li>
-      <li><NuxtLink to="">Historical data</NuxtLink></li>
+      <li :class="currentPage === 'control' ? 'bordered' : ''">
+        <NuxtLink to="/control">Control panel</NuxtLink>
+      </li>
+      <li :class="currentPage === 'history' ? 'bordered' : ''">
+        <NuxtLink to="/history">Historical data</NuxtLink>
+      </li>
+      <li class="disabled"><NuxtLink to="/">Planner</NuxtLink></li>
     </ul>
   </aside>
 </template>
+
+<script>
+import { computed, defineComponent, useRoute } from '@nuxtjs/composition-api'
+
+export default defineComponent({
+  setup() {
+    const route = useRoute()
+    const currentPage = computed(() => route.value.name)
+
+    return { currentPage }
+  },
+})
+</script>
+>
