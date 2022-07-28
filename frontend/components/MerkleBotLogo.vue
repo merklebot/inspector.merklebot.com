6ca602a91891da8f0b091a54e10ed6a31d8a8f23
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-07-28 20:35:32 +0300

    Frontend: update logo

--- frontend/components/MerkleBotLogo.vue
+++ frontend/components/MerkleBotLogo.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="text-3xl">
-    <img class="h-10 w-10 mx-2 inline" alt="MerkleBot Logo" src="/favicon.ico" />
-    MerkleBot
+    <img
+      class="h-10 w-10 mx-2 inline"
+      alt="MerkleBot Logo"
+      src="/favicon.ico"
+    />
+    MerkleBot Inspector
   </div>
 </template>
