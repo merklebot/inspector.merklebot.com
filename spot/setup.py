b5ecc0ec42060bc4ab9f487003ccfb525c6d329b
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-07-28 15:58:18 +0300

    Spot: add setup.py

---
+++ spot/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+
+setup(
+    name="merklebot_inspector",
+    packages=["merklebot_inspector"],
+    description="MerkleBot Inspector module to run on SpotCORE.",
+    python_requires=">=3.8,<3.9",
+)
