335147580958d21e929d38687c6f551f3a9aaa13
Alisher A. Khassanov <a.khssnv@gmail.com>
2022-07-29 11:24:33 +0300

    Frotend: remove tutorial, add hero on index

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,18 @@
 <template>
-  <Tutorial />
+  <div class="hero h-full w-full">
+    <div class="hero-content text-center">
+      <div class="w-full">
+        <h1 class="text-5xl font-bold">Inspector</h1>
+        <p class="py-6">
+          MekrleBot Inspector is an application for Boston Dynamics Spot robot
+          inspections use case.
+        </p>
+        <NuxtLink to="control">
+          <button class="btn btn-primary">Open Control Panel</button>
+        </NuxtLink>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
